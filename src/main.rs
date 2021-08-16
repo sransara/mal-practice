72d62ae7e8e837c5f300aeb9b0353611d6c8b58f
Samodya Abey <git@sransara.com>
2021-08-16 02:15:37 -0500

    stage 0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+fn read(editor: &mut Editor<()>) -> Result<String, ReadlineError> {
+    editor.readline("> ")
+}
+
+fn eval(input: &str) -> &str {
+    return input;
+}
+
+fn print(input: &str) {
+    println!("{}", input)
+}
+
+fn repl(mut editor: Editor<()>) {
+    loop {
+        let readline = read(&mut editor);
+        match readline {
+            Ok(line) => {
+                editor.add_history_entry(line.as_str());
+                let output = eval(line.as_str());
+                print(output);
+            },
+            Err(_) => {
+                break
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let readline_editor = Editor::<()>::new();
+    repl(readline_editor)
 }
