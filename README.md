c6cef98e728655d6dc726c70e638713ae7c282bc
Samodya Abeysiriwardane <git@sransara.com>
2024-05-05 00:04:57 -0500

    Update docs

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # mal-practice
 My attempt of https://github.com/kanaka/mal/
+
+- [x] eval "def!"
+- [x] eval "let*"
+- [x] eval "macro*"
+- [x] eval "quote"
+- [x] eval "quasiquote"
+- [x] `pr_str` support
+- [x] `if`, `fn*`, `do` support
+- Rust macro to support builtins in ./src/builtins.rs
+- [x] `add`, `list`, `throw` builtins
+
+Sample mal program that can be executed:
+```
+(def! fib (fn* (n)
+  (if (lte n 1)
+    n
+    (add (fib (sub n 1)) (fib (sub n 2))))))
+
+(fib 10)
+```
+
+Sample run:
+```
+cat example/fib_test.mal | cargo run
+
+# output
+Function(User { params: [Symbol("n")], body: List([Symbol("if"), List([Symbol("lte"), Symbol("n"), Integer(1)]), Symbol("n"), List([Symbol("add"), List([Symbol("fib"), List([Symbol("sub"), Symbol("n"), Integer(1)])]), List([Symbol("fib"), List([Symbol("sub"), Symbol("n"), Integer(2)])])])]) })
+EOF
+Integer(55)
+```
--- src/builtins.rs
+++ src/builtins.rs
@@ -7,7 +7,7 @@ macro_rules! builtin {
         $menv.set(
             $name,
             MalType::Function(Function::Native {
-                params: vec![$( $params )*]
+                params: vec![$( $params ), *]
                     .iter()
                     .map(|p: &&str| MalType::Symbol((*p).to_owned()))
                     .collect::<Vec<MalType>>(),
@@ -33,6 +33,28 @@ pub fn stdenv<'a>() -> MalEnv<'a> {
             Ok(MalType::Integer(result))
         } else {
             Err(EvalError::InvalidType("List", args))
+        }
+    });
+
+    builtin!(menv, "sub", ["arg1", "arg2"], |menv| {
+        let arg1 = eval(MalType::Symbol("arg1".to_owned()), menv)?;
+        let arg2 = eval(MalType::Symbol("arg2".to_owned()), menv)?;
+        match (&arg1, &arg2) {
+            (MalType::Integer(arg1), MalType::Integer(arg2)) => Ok(MalType::Integer(arg1 - arg2)),
+            (_, MalType::Integer(_)) => Err(EvalError::InvalidType("Integer", arg1)),
+            (MalType::Integer(_), _) => Err(EvalError::InvalidType("Integer", arg2)),
+            _ => Err(EvalError::InvalidType("Integer", arg1)),
+        }
+    });
+
+    builtin!(menv, "lte", ["arg1", "arg2"], |menv| {
+        let arg1 = eval(MalType::Symbol("arg1".to_owned()), menv)?;
+        let arg2 = eval(MalType::Symbol("arg2".to_owned()), menv)?;
+        match (&arg1, &arg2) {
+            (MalType::Integer(arg1), MalType::Integer(arg2)) => Ok(MalType::Bool(arg1 <= arg2)),
+            (_, MalType::Integer(_)) => Err(EvalError::InvalidType("Integer", arg1)),
+            (MalType::Integer(_), _) => Err(EvalError::InvalidType("Integer", arg2)),
+            _ => Err(EvalError::InvalidType("Integer", arg1)),
         }
     });
 
